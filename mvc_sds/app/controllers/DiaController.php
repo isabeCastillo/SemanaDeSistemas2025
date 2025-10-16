de773f822bca886dc56e23dff3f8ab7d45f5d085
isabeCastillo <cp21026@ues.edu.sv>
2025-10-16 16:57:57 -0600

    feat: agregando al diaController los dias 3, 4 y 5

--- mvc_sds/app/controllers/DiaController.php
+++ mvc_sds/app/controllers/DiaController.php
@@ -14,6 +14,25 @@ class DiaController {
             "descripcion" => "Desarrollo de talleres de manera virtual sobre la toma de decisiones, Laravel y filament y la IA Multimodal."
         ]);
     }
+    public function dia3() {
+        echo $this->view("dia3View", [
+            "title" => "Día 3 - Torneo de Futboll",
+            "descripcion" => "Torneo de Futboll cancelado."
+        ]);
+    }
+    public function dia4() {
+        echo $this->view("dia4View", [
+            "title" => "Día 4 - Feria de logros",
+            "descripcion" => "Día dedicado a la convivencia, concursos y actividades grupales."
+        ]);
+    }
+    public function dia5() {
+        echo $this->view("dia5View", [
+            "title" => "Día 5 - Clausura del Evento",
+            "descripcion" => "Cierre oficial del evento con premiaciones, agradecimientos y despedida; Bingo, ponencias y torneo de video juegos."
+        ]);
+    }
+
 
     private function view($vista, $data = []) {
         extract($data);
