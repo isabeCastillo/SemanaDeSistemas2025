26ea24a88fc93c49901df39809213fe6ffcbba4b
isabeCastillo <cp21026@ues.edu.sv>
2025-10-16 00:49:32 -0600

    feat: corrigiendo estilo y agregando fotografias y videos al primer dia

--- mvc_sds/app/views/dia1View.php
+++ mvc_sds/app/views/dia1View.php
@@ -26,9 +26,9 @@
         </div>
     </nav>
     <main>
-        <h1><?= $descripcion ?></h1>
         <div class="main-content-wrapper">
             <div class="text-column">
+                <h1><?= $descripcion ?></h1>
                 <p>La Semana de Sistema arrancó con una bienvenida inspiradora a cargo de la Ing. Cesar Franco donde en primer lugar se le dio la bienvenida a la mesa de honor, se canto el himno nacional y para dar por inahurado cada uno de los integrantes de dicha mesa dieron palabras de bienvenida.</p>
             </div>
             <div class="carrusel-column">
@@ -47,9 +47,62 @@
                 </div>
             </div>
         </div>
-        <div class="conferencia1"></div>
-        <div class="conferencia2"></div>
-        <div class="hackathon"></div>
+        <div class="conferencia1">
+            <h3>Ponencia #1: "El ABC del Bitcoin"</h3>
+            <div class="conference-media">
+                <img src="/mvc_sds/src/9.jpeg" class="conference-image img-right">
+                <p class="image-caption">Una representación visual de la tecnología Bitcoin.</p>
+            </div>
+            <p>Entonces, ¿qué decidimos nosotros? ¿Entender cómo funciona el dinero o realmente pasarle a esa persona? Créanme que los sistemas políticos del mundo prefieren personas obedientes. Personas que hacen de regla. Yo no estoy acá para decirte sí, todo está bien. Estoy hablando de un tema de Bitcoin que necesito que te haga revolución en la mente. Si seas una persona crítica, dice, pa, ¿por qué lo vas a usar? La gente me dice, Miguel, enséñame el ritmo, enséñame cómo hacer un café. Yo no les enseño nada. Este tema no es para sentarte y estafarte. No es para salir en TikTok diciendo, mándenme dinero. Es un tema que realmente requiere tiempo, chicos, porque tiene que ver con economía.</p>
+            <p>Louis Momice, que es uno de los pioneros ahí, dijo que el dinero es el bien más comercializable de las personas. Que las personas adquieren porque quieren ofrecerles gastos posteriores de intercambio personal, haciendo referencia que el valor del dinero no solamente tiene que ver con gastarlo ya, sino que se analiza y se piensa posteriormente, a futuro. Y es lo que la mayoría de las personas no piensan. Por qué les hablo de esto?Porque el tema de Bitcoin no solamente nace porque a alguien se le ocurrió hacer un tipo de tecnología. Aparte, ustedes saben que la palabra tecnología, ¿verdad? En el griego, technet o logistudio, tiene que ver no con lo que entendemos ahorita de los sistemas, sino que tiene que ver con la técnica, con el arte. Con todo eso nace la palabra tecnología.</p>
+            <p>Bitcoin no nació como, hey, alguien se le ocurrió, despertó y voy a crear una tecnología ahorita que sea una moneda digital, sino que lleva una filosofía detrás. Y esa filosofía parte de algo. Una economía quebrada en el mundo, no desde hace poco, sino que si usted se pone a estudiar esa economía quebrada, viene desde mucho antes incluso que los años 80. Si la Reserva Federal fue creada en 1913, formalmente, desde ahí se comienza a desquebrar muchas cosas en el mundo, económicamente hablando. Entonces, la economía austriaca de economía y solamente quiero compartir algo contigo así a grosor modo para que entendamos más o menos de qué se trata. Porque todo eso tiene una consecuencia. O sea, si hablamos de economía, tiene una situación. Y solamente lo voy a leer acá, solo para hacerlo. Vamos a ver algunos puntos claves de la economía que inicialmente casi en todos los países se están usando.</p>
+            <p>Ahora yo le pregunto a usted, que ya sé que tiene mucho conocimiento aquí en la universidad. Si usted guarda 10 mil dólares ahorita en 2025, ¿cuánto creen que va a valer los 10 mil dólares en 2030? Si obviamente el dinero va perdiendo valor todo el tiempo. Entonces, guardar dólares, y esto lo vamos a lo mejor a dar en el tema de tecnología, guardar dólares debajo del colchón es un problema.</p>
+            <p>Ha sido una evolución la sal, un tiempo, fue de mucho valor. De hecho, ahí viene la palabra salario que nosotros usamos, salario mínimo. Roma le pagaba a sus funcionarios con sal, porque la sal era muy escasa. Casi nadie tenía acceso a las playas, lo que es la sociedad sobre Roma. Ponía gente esclava a trabajar, pero luego se hizo bastante común la sal, y hoy nos damos el humo de tirarla, de botarla, no hay ningún problema. Pero un tiempo, fue algo de valor, un dinero muy importante.</p>
+        </div>
+        <div class="conferencia2">
+            <h3>Ponencia #2: "Inteligencia Artificial generativa, de la idea a la acción"</h3>
+            <div class="conference-media">
+                <img src="/mvc_sds/src/ia.jpeg" class="conference-image img-right">
+                <p class="image-caption">La IA, una herramienta que nos impulsa a ser más eficientes.</p>
+            </div>
+            <p>Entonces ahí es donde en realidad determinan que independientemente de la regla que nos estemos formando exactamente, la inteligencia artificial sí tiene un impacto. A partir de esto entonces decimos, la guía no está reemplazando, nos está ayudando a ser más eficientes en nuestro trabajo.  Si nosotros no sabemos cómo utilizarlas, en qué función utilizarlas, pero decimos, ¿qué es la inteligencia artificial? Sí, Algoritmos, ¿qué más? Datos, modelos, exactamente, ¿qué más? En este sentido, si nos vamos a la RAE, en la definición que tenía en 2021 decía que es la capacidad de entender, comprender y es la capacidad de resolver problemas. Entender y la capacidad de comunicarnos y resolver problemas. Y aquí hay un debate álgido entre los ingenieros, entre los filósofos y entre los científicos de si llamar inteligencia artificial es en realidad un nombre comercial o tiene que ver con la practicalidad de esto.
+                Porque dicen los filósofos que es antropomorfizar. Repitan conmigo, antropomorfizar. Pregunto, ¿qué es antropomorfizar? Antropomorfizar es adjudicar características humanas a un objeto, a una tecnología.</p>
+            <p> Nadie imaginaba que iban a venir a asimilar o a enaltar el pensamiento humano. La parte de la cognición, ese proceso de pensar y de simular el pensamiento humano. Cuando nosotros vemos, decimos, la capacidad de conocer, la capacidad de comprender, la capacidad de tener datos, información y conocimiento, no creíamos, nadie se reesperaba que lo iban a ser los charlos.
+                Porque todos pensábamos que iban a ser robots humanoides, que iban a tomar ese tipo de decisiones. Ahí tenemos la hipótesis. En cripto funciona la hipótesis, para poder comprar y vender cuando usted tenga ascenso y descenso.
+                Y entonces también funciona en la parte de inteligencia artificial, por supuesto. Pero viene y reemplaza, o cuando menos simula, la capacidad de pensar. Pero repito, nosotros como seres biológicos, como inteligencia de carbono, tenemos la capacidad de entender, conocer, comprender y aprender.
+                La inteligencia artificial solo tiene información. Que si vamos a competir con ella, pues no debería. Como seres humanos tenemos la capacidad, si somos públicos, de estudiar entre 130 y 742 libros en nuestra vida.
+                La inteligencia artificial, si vemos los patentes con los que ha sido entrenada, es la capacidad de haber aprendido sobre 10 millones de libros. Por cada libro que leemos, nosotros solo retenemos el 10%. La inteligencia artificial almacena el 100% de la información de cada libro.</p>
+        </div>
+        <div class="hackathon">
+    <h3>Hackathon</h3>
+    <p>Se presentaron exactamente 5 propuestas de diseño para ayudar a solucionar distintas porblematicas tanto educativas como sociales.</p>
+    
+    <div class="proyectos-galeria">
+        <div class="proyecto-item">
+            <img src="/mvc_sds/src/odon.jpeg" alt="Proyecto 1">
+            <p>Sistema de citas odontologicas</p>
+            <p>Premio Segundo lugar</p>
+        </div>
+        <div class="proyecto-item">
+            <img src="/mvc_sds/src/asis.jpeg" alt="Proyecto 2">
+            <p>Gestion de asistencia para asistentes a confencias</p>
+        </div>
+        <div class="proyecto-item">
+            <img src="/mvc_sds/src/bin.jpeg" alt="Proyecto 3">
+            <p>Bingo</p>
+            <p>Premio Primer lugar</p>
+        </div>
+        <div class="proyecto-item">
+            <img src="/mvc_sds/src/vete.jpeg" alt="Proyecto 4">
+            <p>Control de veterinaria</p>
+            <p>Premio Tercer lugar</p>
+        </div>
+        <div class="proyecto-item">
+            <img src="/mvc_sds/src/notas.jpeg" alt="Proyecto 5">
+            <p>Sistema de control de notas de estudiantes</p>
+        </div>
+    </div>
+</div>
     </main>
     <footer>
         <div class="footer-content">
